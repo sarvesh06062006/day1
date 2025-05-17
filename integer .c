1051636587a5eb52bcd743ee9e3a8a3a4afb0cb4
sarvesh06062006 <sarveshwarana06@gmail.com>
2025-05-17 12:28:04 +0530

    Create integer .c

---
+++ integer .c
@@ -0,0 +1,17 @@
+ 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    int b;
+    b=6;
+    int temp =a;
+    a=b;
+    b=temp;
+    printf("%d ",a);
+    printf("%d ",b);
+    return 0;
+}
