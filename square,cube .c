69d90963c14c5b188b27fb798399f8c47d99556c
sarvesh06062006 <sarveshwarana06@gmail.com>
2025-05-17 12:52:47 +0530

    Create square,cube .c

---
+++ square,cube .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int s;
+    int c;
+    printf("Enter a number");
+    scanf("%d",&n);
+    printf("Enter square");
+    scanf("%d",&s);
+    printf("Enter cube");
+    scanf("%d",&c);
+    int square = 4;
+    int cube = 5;
+    printf("square = %d\n",square);
+    printf("cube = %d\n",cube);
+    return 0;
+}
